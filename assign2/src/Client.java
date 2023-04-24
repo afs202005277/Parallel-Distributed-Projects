abbb879fbde8a61686a20d1311832afc1a32b412
Andre Sousa <up202005277@up.pt>
2023-04-24 12:03:55 +0100

    updated client

--- assign2/src/Client.java
+++ assign2/src/Client.java
@@ -4,70 +4,88 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Client {
     public static void main(String[] args) throws IOException {
 
         SocketChannel socketChannel = SocketChannel.open();
         socketChannel.connect(new InetSocketAddress("localhost", 8080));
+        socketChannel.configureBlocking(false);
         Scanner scanner = new Scanner(System.in);
         ByteBuffer buffer = ByteBuffer.allocate(4096);
         String token = "";
         boolean canWrite = true;
+        BlockingQueue<String> inputQueue = new LinkedBlockingQueue<>();
+        new Thread(() -> {
+            while (true) {
+                if (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    inputQueue.offer(line);
+                }
+            }
+        }).start();
+        boolean msgPrinted = false;
         do {
             buffer.clear();
             String message = "";
             if (canWrite) {
-                System.out.print("Enter a message to send: ");
-                message = scanner.nextLine();
-                if (Objects.equals(message, "")) {
-                    break;
+                if (!inputQueue.isEmpty()) {
+                    message = inputQueue.poll();
+                    msgPrinted = false;
                 }
-                byte[] bytesToSend = message.getBytes();
-                buffer.put(bytesToSend);
-                buffer.flip();
-                socketChannel.write(buffer);
-                buffer.clear();
+                if (!msgPrinted) {
+                    byte[] bytesToSend = message.getBytes();
+                    buffer.put(bytesToSend);
+                    buffer.flip();
+                    socketChannel.write(buffer);
+                    buffer.clear();
+                }
             }
-            buffer.put(0, new byte[buffer.limit()]);
-            socketChannel.read(buffer);
-            String tmp = new String(buffer.array()).trim();
-            if (tmp.contains("DISCONNECT")){
-                System.out.println(tmp.substring(0, tmp.indexOf("DISCONNECT")));
-                break;
-            }
-            if (canWrite) {
-                if (message.startsWith("login") || message.startsWith("register")) {
-                    if (!tmp.startsWith("Error") && !tmp.startsWith("Usage")) {
-                        token = tmp.substring(tmp.indexOf(": ") + 2, tmp.indexOf("\n"));
-                        final String tok = token;
-                        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                                try {
-                                    if (socketChannel.isOpen()){
-                                        buffer.clear();
-                                        buffer.put(new byte[4096]);
-                                        buffer.put(0, ("logout " + tok).getBytes());
-                                        buffer.flip();
-                                        socketChannel.write(buffer);
-                                        buffer.clear();
-                                        socketChannel.read(buffer);
-                                        socketChannel.close();
-                                    }
-                                } catch (IOException e) {
-                                    System.out.println("ERROR");
-                                }
-                        }));
-                        if (!tmp.contains("Game Starting!"))
-                            canWrite = false;
+            if (socketChannel.isConnected()) {
+                buffer.put(0, new byte[buffer.limit()]);
+                socketChannel.read(buffer);
+                String tmp = new String(buffer.array()).trim();
+                if (!tmp.isBlank()) {
+                    if (tmp.contains("DISCONNECT")) {
+                        System.out.println(tmp.substring(0, tmp.indexOf("DISCONNECT")));
+                        break;
                     }
+                    if (canWrite) {
+                        if (message.startsWith("login") || message.startsWith("register")) {
+                            if (!tmp.startsWith("Error") && !tmp.startsWith("Usage")) {
+                                token = tmp.substring(tmp.indexOf(": ") + 2, tmp.indexOf("\n"));
+                                final String tok = token;
+                                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                                    try {
+                                        if (socketChannel.isOpen()) {
+                                            buffer.clear();
+                                            buffer.put(new byte[4096]);
+                                            buffer.put(0, ("logout " + tok).getBytes());
+                                            buffer.flip();
+                                            socketChannel.write(buffer);
+                                            buffer.clear();
+                                            socketChannel.read(buffer);
+                                            socketChannel.close();
+                                        }
+                                    } catch (IOException e) {
+                                        System.out.println("ERROR");
+                                    }
+                                }));
+                                if (!tmp.contains("Game Starting!"))
+                                    canWrite = false;
+                            }
+                        }
+                    } else {
+                        if (tmp.contains("Game Starting!"))
+                            canWrite = true;
+                    }
+
+
+                    System.out.println(tmp);
                 }
             }
-            else {
-                if (tmp.contains("Game Starting!"))
-                    canWrite = true;
-            }
-
-            System.out.println(tmp);
         } while (true);
         buffer.clear();
         buffer.put(("logout " + token).getBytes());
@@ -76,7 +94,6 @@ public class Client {
         buffer.clear();
         buffer.put(new byte[buffer.limit()]);
         socketChannel.read(buffer);
-        System.out.println("Message received: " + new String(buffer.array()).trim());
         socketChannel.close();
     }
 
--- assign2/src/Game.java
+++ assign2/src/Game.java
@@ -108,28 +108,28 @@ public class Game {
     private void game_logic(String username) {
         Random rand = new Random();
         String message_action = "Walked";
-        if (rand.nextFloat() < 0.01) {
+        if (rand.nextFloat() < 0.10) {
             message_action = "Planted the bomb";
             this.add_score(username, 3);
             if (usernames.indexOf(username) < numPlayers / 2)
                 game_score += 3;
             else
                 game_score -= 3;
-        } else if (rand.nextFloat() < 0.05) {
+        } else if (rand.nextFloat() < 0.50) {
             message_action = "Killed a player";
             this.add_score(username, 2);
             if (usernames.indexOf(username) < numPlayers / 2)
                 game_score += 2;
             else
                 game_score -= 2;
-        } else if (rand.nextFloat() < 0.15) {
+        } else if (rand.nextFloat() < 0.80) {
             message_action = "Assisted on a kill";
             this.add_score(username, 1);
             if (usernames.indexOf(username) < numPlayers / 2)
                 game_score += 1;
             else
                 game_score -= 1;
-        } else if (rand.nextFloat() < 0.10) {
+        } else if (rand.nextFloat() < 0.40) {
             message_action = "Player died";
         }
         this.message_for_server.add(message_action);
