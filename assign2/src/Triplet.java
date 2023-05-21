b7a9d4a1a243fe285cf0985f444d4d84d8242860
Andre Sousa <up202005277@up.pt>
2023-05-21 15:11:25 +0100

    triplet comments

--- assign2/src/Triplet.java
+++ assign2/src/Triplet.java
@@ -1,23 +1,51 @@
+/**
+ The Triplet class represents a generic container that holds three values of different types.
+ It provides getter methods to access the values.
+
+ @param <T1> The type of the first value.
+ @param <T2> The type of the second value.
+ @param <T3> The type of the third value.
+ */
 public class Triplet<T1, T2, T3> {
     private final T1 val1;
     private final T2 val2;
 
     private final T3 val3;
 
+    /**
+     Constructs a Triplet object with the specified values.
+     @param val1 The first value.
+     @param val2 The second value.
+     @param val3 The third value.
+     */
     public Triplet(T1 val1, T2 val2, T3 val3) {
         this.val1 = val1;
         this.val2 = val2;
         this.val3 = val3;
     }
 
+    /**
+     Returns the first value.
+     @return The first value.
+     */
     public T1 getVal1() {
         return val1;
     }
 
+    /**
+
+     Returns the second value.
+     @return The second value.
+     */
     public T2 getVal2() {
         return val2;
     }
 
+    /**
+
+     Returns the third value.
+     @return The third value.
+     */
     public T3 getVal3() {
         return val3;
     }
